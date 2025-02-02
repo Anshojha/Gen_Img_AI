9b686adbd46562e27791e66c4a5bef23ce003152
Anshojha <anshojha24@gmail.com>
2025-02-02 22:54:30 +0530

    Completed home page

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import Result from './pages/Result'
 import Home from './pages/Home'
 import BuyCredit from './pages/BuyCredit'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 
 const App = () => {
   return (
     <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50">
-        <Navbar />
+      <Navbar />
       <Routes>
      
         <Route path="/" element={<Home />} />
         <Route path="/result" element={<Result />} />
         <Route path="/buy-credit" element={<BuyCredit />} />
       </Routes>
+      <Footer />
     </div>
   )
 }
